2ddb1cfacd94f50458e3775477b9d56db4b9d58a
baxter-t <baxter.t98@gmail.com>
2020-09-15 11:46:45 +1000

    Restructure

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import time
+import socket
 import pyshark
-import mappings
+from mappings.protocols import *
+
+from stream import Stream
+from packet import TCPPacket, UDPPacket
+
 
 '''
     proto: 0.005927314613696935
@@ -14,13 +20,44 @@ import mappings
     ct_srv_dst: 0.10490771665017604
 '''
 
-cap = pyshark.LiveCapture(interface='en0', bpf_filter='ip')
+IP_ADDRESS = '192.168.1.104'
+streams = {}
 
-sbytes_history = {}
+def parse(pkt):
 
-def print_callback(pkt):
-    print(pkt)
+    # All on pkt in, deal with connections from that IP
+    if pkt.ip.proto == '6':
+        print("Parsing TCP packet")
+        if not streams.get(pkt.tcp.stream):
+            client = pkt.ip.src if pkt.ip.dst == IP_ADDRESS else pkt.ip.dst
+            client_port = pkt.tcp.srcport if pkt.ip.dst == IP_ADDRESS else pkt.tcp.dstport
+            server_port = pkt.tcp.dstport if pkt.ip.dst == IP_ADDRESS else pkt.tcp.srcport
+
+            streams[pkt.tcp.stream] = Stream(pkt.ip.proto, client, client_port, IP_ADDRESS, server_port)
         
+        streams[pkt.tcp.stream].add_packet(pkt)
 
-cap.apply_on_packets(print_callback)
+    elif pkt.ip.proto == '17':
+        print("Parsing UDP packet")
+        if not streams.get(pkt.udp.stream):
+            client = pkt.ip.src if pkt.ip.dst == IP_ADDRESS else pkt.ip.dst
+            client_port = pkt.udp.srcport if pkt.ip.dst == IP_ADDRESS else pkt.udp.dstport
+            server_port = pkt.udp.dstport if pkt.ip.dst == IP_ADDRESS else pkt.udp.srcport
+
+            streams[pkt.udp.stream] = Stream(pkt.ip.proto, client, client_port, IP_ADDRESS, server_port)
+        
+        streams[pkt.udp.stream].add_packet(pkt)
+
+    else: 
+        pkt.pretty_print()
+
+    if len(streams.keys()) == 5:
+        for x in streams:
+            streams[x].pretty_print()
+
+        exit()
+
+
+cap = pyshark.LiveCapture(interface='en0', bpf_filter='ip')
+cap.apply_on_packets(parse)
 
---
+++ stream.py
@@ -0,0 +1,56 @@
+import time
+
+from packet import *
+
+class Stream:
+    def __init__(self, proto, client_ip, client_port, server_ip, server_port):
+        self.pkts = []
+        self.proto = proto
+        self.client = client_ip
+        self.server = server_ip
+        self.client_port = client_port
+        self.server_port = server_port
+        self.start_time = time.time()
+        self.last_time = time.time()
+
+        self.outbound_pkt_count = 0
+        self.inbound_pkt_count = 0
+        self.outbound_bytes = 0
+        self.inbound_bytes = 0
+        self.outbound_bytes_mean = 0
+        self.inbound_bytes_mean = 0
+
+    def add_packet(self, pkt):
+        if pkt.ip.proto == '6':
+            new_packet = TCPPacket(pkt)
+        else :
+            new_packet = UDPPacket(pkt)
+
+        self.last_time = time.time()
+
+        if pkt.ip.src == self.server:
+            # Outbound
+            self.outbound_pkt_count += 1
+            self.outbound_bytes += int(new_packet.length)
+            self.outbound_bytes_mean = self.outbound_bytes / self.outbound_pkt_count
+        elif pkt.ip.dst == self.server: 
+            # Inbound
+            self.inbound_pkt_count += 1
+            self.inbound_bytes += int(new_packet.length)
+            self.inbound_bytes_mean = self.inbound_bytes / self.inbound_pkt_count
+
+    def pretty_print(self):
+        print(f'{"TCP" if self.proto == "6" else "UDP"}Stream: {self.client}:{self.client_port} -> :{self.server_port}')
+        print(f'    {self.client}')
+        print(f'    {self.client_port}')
+        print(f'    {self.server_port}')
+        print(f'    {self.start_time}')
+        print(f'    {self.last_time}')
+        print(f'    {self.outbound_pkt_count}')
+        print(f'    {self.inbound_pkt_count}')
+        print(f'    {self.outbound_bytes}')
+        print(f'    {self.inbound_bytes}')
+        print(f'    {self.outbound_bytes_mean}')
+        print(f'    {self.inbound_bytes_mean}')
+
+
